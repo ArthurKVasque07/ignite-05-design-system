64eeee7871181f93af4d88b2a14aaddd3693f938
Arthur Kavagouth Vasque <arthurkvasque.eng@outlook.com>
2023-02-14 12:48:38 -0300

    finish lesson 3

---
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -0,0 +1,27 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Heading, HeadingProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Heading',
+  component: Heading,
+  args: {
+    children: 'Custom title',
+  },
+} as Meta<HeadingProps>
+
+export const Primary: StoryObj<HeadingProps> = {}
+
+export const CustomTag: StoryObj<HeadingProps> = {
+  args: {
+    children: 'H1 heading',
+    as: 'h1',
+  },
+  parameters: {
+    docs: {
+      description: {
+        story:
+          "Por padrão heading sempre sera h2, mas com a prop 'as' da para alterar",
+      },
+    },
+  },
+}
---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,19 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children: 'teste de texto',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'strong text',
+    as: 'strong',
+  },
+}
